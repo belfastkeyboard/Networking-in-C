ae199032bb480ae3800f1b95a36ffffaddfb3c87
belfastkeyboard <ryanftoal@gmail.com>
2024-04-07 20:30:39 +0100

    Added networking functions

--- utils/utils.c
+++ utils/utils.c
@@ -18,13 +18,165 @@ void PrintWSAErrorMessage(int error)
     }
 }
 
+STATUS NetworkWSAInitialisation(void)
+{
+    WSADATA wsa_data;
+    WORD ver;
+    int result;
+    
+    ver = MAKEWORD(2, 2);
+
+    result = WSAStartup(ver, &wsa_data);
+
+    if (result == WSA_SUCCESS)
+    {
+        return result;
+    }
+    else
+    {
+        PrintWSAErrorMessage(result);
+        return result;
+    }
+}
+STATUS NetworkWSACleanup(void)
+{
+    int result, error;
+
+    result = WSACleanup();
+
+    if (result == WSA_SUCCESS)
+    {
+        return result;
+    }
+    else
+    {
+        error = WSAGetLastError();
+        PrintWSAErrorMessage(error);
+        return result;
+    }
+}
+
+STATUS NetworkCreateSocket(SOCKET* sockfd, int af, int type, int protocol)
+{
+    *sockfd = socket(af, type, protocol);
+
+    if (*sockfd == INVALID_SOCKET)
+    {
+        warn("Socket creation failed!", 0);
+        PrintWSAErrorMessage(WSAGetLastError());
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        return EXIT_SUCCESS;
+    }
+}
+STATUS NetworkCloseSocket(SOCKET sockfd)
+{
+    int result, error;
+
+    result = closesocket(sockfd);
+
+    if (result == WSA_SUCCESS)
+    {
+        return result;
+    }
+    else
+    {
+        error = WSAGetLastError();
+        PrintWSAErrorMessage(error);
+        return result;
+    }
+}
+
+STATUS NetworkBindSocket(SOCKET sockfd, struct sockaddr_in* addr, int addrlen)
+{
+    int result, error;
+
+    result = bind(sockfd, (const struct sockaddr*)addr, addrlen);
+
+    if (result == WSA_SUCCESS)
+    {
+        return result;
+    }
+    else
+    {
+        error = WSAGetLastError();
+        PrintWSAErrorMessage(error);
+        return result;
+    }
+}
+
+STATUS NetworkListenSocket(SOCKET sockfd, int backlog)
+{
+    int result, error;
+
+    result = listen(sockfd, backlog);
+
+    if (result == WSA_SUCCESS)
+    {
+        return result;
+    }
+    else
+    {
+        error = WSAGetLastError();
+        PrintWSAErrorMessage(error);
+        return result;
+    }
+}
+STATUS NetworkWaitForConnection(struct sockaddr_in* addr, SOCKET serv, SOCKET* recv)
+{
+    int client_len, error;
+    char host[NI_MAXHOST]; // Client remote name
+    char service [NI_MAXSERV]; // Client service (i.e. port)
+    DWORD len;
+
+    memset(addr, 0, sizeof(*addr));
+
+    client_len = sizeof(*addr);
+    *recv = accept(serv, (struct sockaddr*)addr, &client_len);
+
+    if (*recv == INVALID_SOCKET)
+    {
+        error = WSAGetLastError();
+        PrintWSAErrorMessage(error);
+        return EXIT_FAILURE;
+    }
+
+    memset(host, 0, NI_MAXHOST);
+    memset(service, 0, NI_MAXSERV);
+
+    if (getnameinfo((const struct sockaddr*)&addr, sizeof(addr), host, NI_MAXHOST, service, NI_MAXSERV, 0))
+    {
+
+        len = NI_MAXHOST;
+
+        WSAAddressToStringA((struct sockaddr*)&addr, sizeof(addr), NULL, host, &len);
+        info("%s connected on port %s", host, ntohs(addr->sin_port));
+    }
+    else
+    {
+        info("%s connected on port %s", host, service);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void NetworkConstructSockaddr_in(struct sockaddr_in* addr, short fam, u_short port, u_long S_addr)
+{
+    memset(addr, 0, sizeof(*addr));
+    addr->sin_family = fam;
+    addr->sin_port = htons(port);
+    addr->sin_addr.S_un.S_addr = S_addr;
+}
+
 STATUS CreateUDPSocket(SOCKET* sockfd)
 {
     *sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
     if (*sockfd == INVALID_SOCKET)
     {
-        warn("Socket creation failed!");
+        warn("Socket creation failed!", 0);
         PrintWSAErrorMessage(WSAGetLastError());
         return EXIT_FAILURE;
     }
--- utils/utils.h
+++ utils/utils.h
@@ -1,15 +1,30 @@
-#include <winsock2.h>
-#include <stdio.h>
+#ifndef NETWORKING_UTILITIES
+#define NETWORKING_UTILITIES 1
 
-#define PORT 12345
-#define NON_BLOCKING_MODE 1
-#define LOCALHOST "127.0.0.1"
-#define BUFFER_LEN 1024
+#define _WIN32_WINNT _WIN32_WINNT_VISTA
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#include "status.h"
+
+#define PORT 54000
+#define BUFFER_LEN 4096
+#define WSA_SUCCESS 0
 
 typedef int STATUS;
 
 void PrintWSAErrorMessage(int error);
 
-STATUS CreateUDPSocket(SOCKET* sockfd);
+STATUS NetworkWSAInitialisation(void);
+STATUS NetworkWSACleanup(void);
 
-void GetIPAddr(char* ipaddr);
+STATUS NetworkCreateSocket(SOCKET* sockfd, int af, int type, int protocol);
+STATUS NetworkCloseSocket(SOCKET sockfd);
+STATUS NetworkBindSocket(SOCKET sockfd, struct sockaddr_in* addr, int addrlen);
+STATUS NetworkListenSocket(SOCKET sockfd, int backlog);
+STATUS NetworkWaitForConnection(struct sockaddr_in* addr, SOCKET serv, SOCKET* recv);
+
+void NetworkConstructSockaddr_in(struct sockaddr_in* addr, short fam, u_short port, u_long S_addr);
+
+#endif 
